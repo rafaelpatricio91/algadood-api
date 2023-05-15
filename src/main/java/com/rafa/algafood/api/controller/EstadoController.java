d71f577f06b4a9f25775ff077d82377e8de2337c
Rafa Patricio <rafaelpatricio91@gmail.com>
2023-05-15 19:11:04 -0300

    add produces json no estado

--- src/main/java/com/rafa/algafood/api/controller/EstadoController.java
+++ src/main/java/com/rafa/algafood/api/controller/EstadoController.java
@@ -3,6 +3,7 @@ package com.rafa.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +12,13 @@ import com.rafa.algafood.domain.model.Estado;
 import com.rafa.algafood.domain.repository.EstadoRepository;
 
 @RestController 
-@RequestMapping(value = "/estados")
+@RequestMapping(value = "/estados") //,produces = MediaType.APPLICATION_JSON_VALUE)
 public class EstadoController {
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
 
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Estado> listar() {
 		return estadoRepository.listar();
 	}
